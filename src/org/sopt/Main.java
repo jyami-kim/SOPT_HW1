d8b556901b15d227742f6709856b0b487b858d38
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2018-10-23 21:07:47 +0900

    second commit

--- src/org/sopt/Main.java
+++ src/org/sopt/Main.java
@@ -1,24 +1,46 @@
 package org.sopt;
 
+import org.sopt.model.Department;
+import org.sopt.model.Professor;
 import org.sopt.model.Student;
+import org.sopt.model.University;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
+        final ArrayList<ArrayList> listSet = new ArrayList<ArrayList>();
         final ArrayList<Student> studentList = new ArrayList<Student>();
+        final ArrayList<University> universities = new ArrayList<University>();
+        final ArrayList<Department> departments = new ArrayList<Department>();
+        final ArrayList<Professor> professors = new ArrayList<Professor>();
 
-        final Student student1 = new Student();
-        final Student student2 = new Student();
-        final Student student3 = new Student();
+        for(int i = 0; i < 3; i++) {
+            studentList.add(new Student());
+            universities.add(new University());
+            departments.add(new Department());
+            professors.add(new Professor());
+        }
 
-        studentList.add(student1);
-        studentList.add(student2);
-        studentList.add(student3);
+        listSet.add(studentList);
+        listSet.add(universities);
+        listSet.add(departments);
+        listSet.add(professors);
 
-        for(final Student s: studentList){
+        for(Student s: studentList) {
             System.out.println(s.toString());
         }
+        for(University u : universities){
+            System.out.println(u.toString());
+        }
+        for(Department d : departments){
+            System.out.println(d.toString());
+        }
+        for(Professor p : professors){
+            System.out.println(p.toString());
+        }
+
     }
 }
